b968875a4773ec3049ee921a5b065401cb5fce13
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-02-21 11:29:12 +0100

    refactor: moved each configuration into their own packages

--- index.js
+++
@@ -1,7 +0,0 @@
-const { isPackageExists } = require("local-pkg");
-
-const TS = isPackageExists("typescript");
-
-module.exports = {
-  extends: [TS ? "./typescript" : "./basic"]
-};
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,12 @@
+const { isPackageExists } = require("local-pkg");
+
+const TS = isPackageExists("typescript");
+
+if (!TS) {
+  console.warn(
+    "[@luxass/eslint-config] TypeScript is not installed, fallback to JS only."
+  );
+}
+module.exports = {
+  extends: [TS ? "@luxass/eslint-config-ts" : "@luxass/eslint-config-js"]
+};
