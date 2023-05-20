6cfda3e3f30e797846d2877ef157a680dc36543c
luxass <lucasnrgaard@gmail.com>
2023-05-20 21:02:31 +0200

    added vue config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,12 +1,5 @@
-const { isPackageExists } = require("local-pkg");
-
-const TS = isPackageExists("typescript");
-
-if (!TS) {
-  console.warn(
-    "[@luxass/eslint-config] TypeScript is not installed, fallback to JS only."
-  );
-}
 module.exports = {
-  extends: [TS ? "@luxass/eslint-config-ts" : "@luxass/eslint-config-js"]
+  extends: [
+    "@luxass/eslint-config-vue"
+  ]
 };
